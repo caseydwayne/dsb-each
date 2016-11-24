8d657c8bcae09412c5b8a9b644229cb4615a7ee6
caseydwayne <caseydwayne@creativedesigninfluence.com>
2016-11-24 04:19:36 -0500

    transitioning to possibly open up a few modules
    imported some more DSB modules
    
    created a few new modules

--- index.js
+++ index.js
@@ -1,39 +1,66 @@
-(function(){
+module.exports = (function(){
+/*---------------------------------------------------------------------*/
   
+  var toArray = require('dsb-to-array');   
+  
+/***********************************************************************/  
   
   /* @method each
    * @param [target] {object|array} will default to 'this'
    * @param method {function}
    * @rest arguments
+   * @param [map] {boolean} if true, maps directly to the object being iterated
    * works on objects || arrays. passes thru the function (1st arg) supplying (value, key, index). returns output of function or original value if no function output.
+   * @changed 11-8-16 to force array keys as true numbers for K (instead of 'K')
    */
   
-  var toArray = require('dsb-to-array');   
+/*---------------------------------------------------------------------*/
    
   var each = function(){
-    var index = 0, 
-            a = toArray(arguments),
-            l = a.length,
-            f,
-            o;
-    //check for classic signature    
-    if( l > 1 ){
-      o = a.shift();
-      f = a.shift();
-    //go native if not
-    } else {
+    var a = toArray(arguments),        
+        i = 0,
+        m = false,
+        f,
+        o;
+    //check for native signature
+    if( typeof( a[0] ) === 'function' ){      
       o = this;
       f = a.shift();
+    //attempt for classic signature
+    } else {      
+      o = a.shift();
+      f = a.shift();
     }
+    
     //check for fn
     if (typeof f !== 'function') throw new Error('each() requires a method');
-    for(var i in o){
-      o[ i ] = f( o[ i ], i, index++, a ) || o[ i ]; 
+    
+    //check for map
+    if( a.length && ( a[0] === true ) ) m = true;
+    
+    //create object to dump iterated data into
+    if( !m ) var x = {}; 
+    
+    for(var n in o){
+      //increase the index
+      i++;
+      //force true number if numerical key
+      if( parseInt(n) === i ) n = i;
+      //result from fn
+      var r = f( o[ n ], n, i );
+      //value (result || original)
+      var v = ( typeof r !== 'undefined' ) ? r : o[n];
+      //map (overwrite) to original or add to dump object
+      m ? o[ n ] = v : x[n] = v;
     }
-    return o;     
-  };
+    
+    return m ? o : x; //y     
   
-  module.exports = each;
-  return each;
+  };
 
+/***********************************************************************/
+
+  return each;
+  
+/*---------------------------------------------------------------------*/
 }());
